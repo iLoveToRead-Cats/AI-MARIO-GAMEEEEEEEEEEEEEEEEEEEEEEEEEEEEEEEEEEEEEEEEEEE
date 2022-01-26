c52c8253314f0c5e2a1fcbfd32021c177ce7a859
iLoveToRead-Cats <75917493+iLoveToRead-Cats@users.noreply.github.com>
2022-01-26 16:23:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
+nose_x="";
+nose_y="";
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav");
+	mario_coin=loadSound("coin.wav");
+	mario_gameover=loadSound("gameover.wav");
+	mario_kick=loadSound("kick.wav");
+	mario_die=loadSound("mario_die");
 	setSprites();
 	MarioAnimation();
-}
+}                  
 
 function setup() {
 	canvas = createCanvas(1240,336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video=createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+	poseNet=ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-	game()
+	game();
+	
 }
-
-
+function modelLoaded() {
+	console.log("model loaded");
+}
+function gotPoses(results) {
+	if (results.length>0) {
+		console.log(results);
+		nose_x=results[0].pose.nose.x;
+		nose_y=results[0].pose.nose.y;
+		console.log("Nose X: " + nose_x);
+		console.log("Nose Y: " + nose_y);
+	}
+}
 
 
 
